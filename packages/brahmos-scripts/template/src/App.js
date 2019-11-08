16bec70519f32281cea8caa4dabb7e7e307c205e
Ameer Jhan <ameerjhanprof@gmail.com>
2019-11-08 19:10:29 +0530

    add customized page

--- packages/brahmos-scripts/template/src/App.js
+++ packages/brahmos-scripts/template/src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/brahmosjs/brahmos"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Brahmos
         </a>
       </header>
     </div>
